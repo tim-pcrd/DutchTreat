0bc712259e40263015cb77eb6c4d163b0803bb38
iamtimp <timp@tuta.io>
2019-01-29 23:17:04 +0100

    Add project files.

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,32 @@
+﻿
+$(document).ready(function() {
+    console.log("Hello Pluralsight");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click",
+        () => {
+            console.log("Buying item");
+        });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click",
+        function() {
+            console.log("You clicked on " + $(this).text());
+        });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click",
+        function() {
+            $popupForm.fadeToggle(1000);
+        });
+
+});
+
+
